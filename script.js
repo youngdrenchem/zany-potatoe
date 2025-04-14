f3c6bf2928ccd534030f3c618d4f981e6dc140be
youngdrenchem <mlharris18@cps.edu>
2025-04-14 14:36:56 +0000

    v1

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let tennessee= document.querySelector(".tennessee");
 let texasAM= document.querySelector(".texasAM");
 let UCF= document.querySelector(".UCF");
-let kentucky= document.querySelector(".Kentucky");
+let kentucky= document.querySelector(".kentucky");
+
+
 
 tennessee.style.display="none";
 texasAM.style.display="none";
@@ -10,29 +12,16 @@ kentucky.style.display="none";
 
 button.onclick=function(){
 console.log("button check");
-};
 
-  let answer1 = input1.value;
+
+  let answer1 = input1.value
   console.log(answer1);
-  let answer2 = input2.value;
+  let answer2 = input2.value
   console.log(answer2);
+}
 
-if(answer1==="2" && answer2==="dolphin"){ 
-    console.log("Tennessee");
+if(answer1==="2" && answer2==="dolphins"){ 
+    console.log("tennessee");
     tennessee.style.display="block";
-    }
-     
-else if(answer1==="2" && answer2==="shark"){ 
-        console.log("texasAM");
-        texasAM.style.display="block";
-    }
-
-if(answer1==="4" && answer2==="dolphin"){ 
-            console.log("Kentucky");
-            kentucky.style.display="block";
-        }
-
-else if(answer1==="4" && answer2==="shark"){ 
-                console.log("UCF");
-                UCF.style.display="block";
-            }
+    kentucky.style.display="none";
+}
